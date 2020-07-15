e118750d67712b2f87c11f37cc27c26edcef36ec
Shubhrajit Sarkar <36480249+Shubhrajit2399@users.noreply.github.com>
2020-07-15 03:15:04 -0400

    Add files via upload

---
+++ Defanning_an_IP.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String defangIPaddr(String address) {
+        String[] a=address.split("");
+        String[] a1=new String[address.length()];
+        StringBuffer sb = new StringBuffer();
+        String s;
+        for(int i=0;i<a.length;i++){
+            if(a[i].equals(".")){
+                a1[i]="["+a[i]+"]";
+            }
+            else{
+                a1[i]=a[i];
+            }
+        }
+        for(int i=0;i<a1.length;i++){
+            sb.append(a1[i]);
+        }
+        s=sb.toString();
+        return s;
+    }
+}
